2fa99141257b30f9408854708a4d3eb7c6a6d1ed
Praneed.K <>
2022-08-23 15:02:10 +0700

    adding a cart context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import {Fragment, useState} from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
     const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,7 +17,7 @@ function App() {
 
 
     return (
-        <Fragment>
+        <CartProvider>
             {
                 cartIsShown &&
                 <Cart
@@ -29,7 +30,7 @@ function App() {
             <main>
                 <Meals/>
             </main>
-        </Fragment>
+        </CartProvider>
     );
 }
 
